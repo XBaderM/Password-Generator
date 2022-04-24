75a71b7cca277230a677576fbc1a8005c6cbb953
Bader Munir <bader.munir18@googlemail.com>
2022-04-24 03:33:12 +0100

    add logic

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,65 +6,76 @@ var generateBtn = document.querySelector("#generate");
 // console.log(passwordlength);
 // function generatePassword(){
 
+//   function askQuestion() {
+//     var choice1 = prompt("a question").toLowerCase;
+//     if (choice1 === option) {
+//          alert("Something");
+//     } else {
+//          alert("that's not possible");
+//          //go back to the original prompt
+//          askQuestion();
+//     }
+// }
+
  
 
 // }
 
 // // Write password to the #password input
-// function writePassword() {
-//   let passwordlength = prompt("pleater number between 8 - 128");
-//   if(passwordlength < 8 || passwordlength > 128){
+function writePassword() {
+  let passwordlength = prompt("pleater number between 8 - 128");
+  if(passwordlength < 8 || passwordlength > 128 || passwordlength === ""){
     
-//     alert("password either to short or too high");
-//   }else if(isNaN(passwordlength)){
-//     alert("please enter numbers");
-//   }else{
-//     // console.log(passwordlength);
-//     let foo = parseInt(passwordlength);
-//     // console.log(foo);
-//     return foo;
-//   }
-// }
-
-function criteria(){
-//  let capital = prompt("would you like to include capital yes or no")
-//  if(capital === "yes"){
-//    console.log(capital)
-//  }
-
-      let capitals = confirm("would you like capitals");
-      if (capitals== true){
-        console.log("you pressed ok");
-      }else{
-      console.log ("you canceled");
-      }
-
-      let lowerCase = confirm("would you like lowercase");
-      if (lowerCase == true){
-        console.log("you pressed ok");
-      }else{
-      console.log ("you canceled");
-      }
-
-      let specials = confirm("would you like special characters");
-      if (specials == true){
-        console.log("you pressed ok");
-      }else{
-      console.log ("you canceled");
-      }
-
-      let numbers = confirm("would you like numbers");
-      if (numbers== true){
-        console.log("you pressed ok");
-      }else{
-      console.log ("you canceled");
-      }
+    alert("password either to short or too high");
+  }else if(isNaN(passwordlength)){
+    alert("please enter numbers");
+  }else{
+    let foo = parseInt(passwordlength);
+    // console.log(foo);
+    return foo;
+  }
 }
 
-criteria();
+// function criteria(){
+// //  let capital = prompt("would you like to include capital yes or no")
+// //  if(capital === "yes"){
+// //    console.log(capital)
+// //  }
+
+//       // let capitals = confirm("would you like capitals");
+//       // if (capitals== true){
+//       //   console.log("you pressed ok");
+//       // }else{
+//       // console.log ("you canceled");
+//       // }
+
+//       // let lowerCase = confirm("would you like lowercase");
+//       // if (lowerCase == true){
+//       //   console.log("you pressed ok");
+//       // }else{
+//       // console.log ("you canceled");
+//       // }
+
+//       // let specials = confirm("would you like special characters");
+//       // if (specials == true){
+//       //   console.log("you pressed ok");
+//       // }else{
+//       // console.log ("you canceled");
+//       // }
+
+//       // let numbers = confirm("would you like numbers");
+//       // if (numbers== true){
+//       //   console.log("you pressed ok");
+//       // }else{
+//       // console.log ("you canceled");
+//       // }
+// }
+
+// criteria();
+writePassword();
 
 function generatePassword(){
-  // writePassword();
+  
 }
 // var len = writePassword();
 // console.log(len);
